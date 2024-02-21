cbc39a1f4579ace8dd73f55314325616ff224e70
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-02-21 10:12:43 -0700

    meme component matches tutorial

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,19 +2,25 @@ import React from "react"
 
 export default function Meme() {
     return (
-        <div className="meme">
-        <div className="form">
-            <div className="input">
-                <label for="topText" className="label">Top text</label>
-                <input type="text" id="topText" placeholder="Shut up" />
+        <main>
+        <form className="form">
+            <div>
+                <label className="form--label">Top text</label>
+                <input 
+                className="form--input" 
+                type="text" 
+                placeholder="Shut up" />
             </div>
-            <div className="input">
-                <label for="bottomText" className="label">Bottom text</label>
-                <input type="text" id="bottomText" placeholder="And take my money" />
+            <div>
+                <label className="form--label">Bottom text</label>
+                <input 
+                className="form--input" 
+                type="text" 
+                id="bottomText" 
+                placeholder="And take my money" />
             </div>
-            
-        </div>
-        <button type="button">Get a new meme image 🖼️</button> 
-        </div>
+            <button className="form--button" type="button">Get a new meme image 🖼️</button> 
+        </form>        
+        </main>
     )
 }
